bb281778e7294fd2861d767bd3bf6d5c3775985a
syifamaulida <syifa.maulida@mhs.itenas.ac.id>
2026-01-07 09:42:44 +0700

    inisiasi proyek aplikasi AritmatikaApp

---
+++ AritmatikaApp/src/aritmatikaapp/AritmatikaApp.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aritmatikaapp;
+
+/**
+ *
+ * @author HP G8
+ */
+public class AritmatikaApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public class Operation {
+        public double add(double a, double b){
+            return a+b;
+        }
+    }
+    
+}
+    
+
